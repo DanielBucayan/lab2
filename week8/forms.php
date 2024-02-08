bb6e59b9c619e81a6fae3105da3de2f70f702ea1
Daniel Bucayan <dpbucayan@student.apc.edu.ph>
2024-02-08 16:01:28 +0800

    updated database

--- week8/forms.php
+++ week8/forms.php
@@ -9,8 +9,8 @@
 
 <?php
 // define variables and set to empty values
-$nameErr = $suggestionErr = $commentErr = "";
-$name = $suggestion = $comment = "";
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["name"])) {
@@ -23,16 +23,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
   }
   
-  if (empty($_POST["suggestion"])) {
-    $suggestionErr = "Suggestion is required";
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
   } else {
-    $suggestion = test_input($_POST["suggestion"]);
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+    
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+      $websiteErr = "Invalid URL";
+    }
   }
 
   if (empty($_POST["comment"])) {
     $comment = "";
   } else {
     $comment = test_input($_POST["comment"]);
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
   }
 }
 
@@ -50,10 +70,19 @@ function test_input($data) {
   Name: <input type="text" name="name" value="<?php echo $name;?>">
   <span class="error">* <?php echo $nameErr;?></span>
   <br><br>
-  Suggestion: <input type="text" name="suggestion" value="<?php echo $suggestion;?>">
-  <span class="error">* <?php echo $suggestionErr;?></span>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website" value="<?php echo $website;?>">
+  <span class="error"><?php echo $websiteErr;?></span>
   <br><br>
   Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
+  <span class="error">* <?php echo $genderErr;?></span>
   <br><br>
   <input type="submit" name="submit" value="Submit">  
 </form>
@@ -62,10 +91,13 @@ function test_input($data) {
 echo "<h2>Your Input:</h2>";
 echo $name;
 echo "<br>";
-echo $suggestion;
+echo $email;
+echo "<br>";
+echo $website;
 echo "<br>";
 echo $comment;
 echo "<br>";
+echo $gender;
 ?>
 
 </section>
@@ -78,23 +110,14 @@ $password = "=latHen97";
 $dbname = "webprogss221";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Form submission handling
 
-    // Form validation and data sanitization
-    $name = mysqli_real_escape_string($conn, trim(htmlspecialchars($_POST["name"]))); // Use all sanitization techniques!
-    $suggestion = mysqli_real_escape_string($conn, trim(htmlspecialchars($_POST["suggestion"])));
-    $comment = mysqli_real_escape_string($conn, trim(htmlspecialchars($_POST["comment"])));
-
-    // Check validation and display errors if needed
-
-    // Database connection with error handling
     $conn = new mysqli($servername, $username, $password, $dbname);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
     // Construct SQL query with variables enclosed in single quotes
-    $sql = "INSERT INTO dpbucayan_myguests (name, suggestion, comment) VALUES ('$name', '$suggestion', '$comment')";
+    $sql = "INSERT INTO dpbucayan_myguests (name, email, website, comment, gender) VALUES ('$name', '$email', '$website', '$comment', '$gender')";
 
     // Execute query and handle errors
     if ($conn->query($sql) === TRUE) {
